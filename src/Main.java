5d1996ac3e2ee977973982a3c165baee90a3070a
paras <parasad612@gmail.com>
2023-10-29 14:03:48 +0100

    working

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Queue;
 
 public class Main {
+    // Lanes
     private static Lane A1 = new Lane("A1", 0.25);
     private static Lane A2 = new Lane("A2", 0.25);
     private static Lane A3 = new Lane("A3", 0.07);
@@ -14,56 +15,68 @@ public class Main {
 
     private static int sizeOfA3 = 3;
 
+    // Lists to save average Dwell Times
+    private static List<Double> dwellA1 = new ArrayList<>();
+    private static List<Double> dwellA2 = new ArrayList<>();
+    private static List<Double> dwellA3 = new ArrayList<>();
+    private static List<Double> dwellB1 = new ArrayList<>();
+    private static List<Double> dwellC1 = new ArrayList<>();
+    private static List<Double> dwellC2 = new ArrayList<>();
+    private static List<Double> dwellD1 = new ArrayList<>();
+
     public static void main(String[] args) {
-        List<Integer> dwellA1 = new ArrayList<>();
-        List<Integer> dwellA2 = new ArrayList<>();
-        List<Integer> dwellA3 = new ArrayList<>();
-        List<Integer> dwellB1 = new ArrayList<>();
-        List<Integer> dwellC1 = new ArrayList<>();
-        List<Integer> dwellC2 = new ArrayList<>();
-        List<Integer> dwellD1 = new ArrayList<>();
 
         for (int k = 0; k < 30; k++){
 
             // 1 SIMULATION
             for (int time = 0; time < 120; time++) {
                 System.out.println("\n-------------------- SECOND " + time + " --------------------");
-                addNewRandomCar(time);      //
-                printLanes();           //
-                removeCars(time);       //
-                printLanes();           //
+                addNewRandomCar(time);      // Adds cars to Lanes
+                printLanes();               // Shows the updated Lanes
+                removeCars(time);           // Removes cars from lanes
+                printLanes();               // Shows the updated Lanes
             }
-            dwellA1.add(getDwellTimes(A1));
-            dwellA2.add(getDwellTimes(A2));
-            dwellA3.add(getDwellTimes(A3));
-            dwellB1.add(getDwellTimes(B1));
-            dwellC1.add(getDwellTimes(C1));
-            dwellC2.add(getDwellTimes(C2));
-            dwellD1.add(getDwellTimes(D1));
-
-//            System.out.println("----------------- DWELL TIME -------------");
-////        System.out.println("A1 : " + A1.exitedCars);
-//            System.out.println("AVG DWELL TIME OF A1: " + getDwellTimes(A1));
-////        System.out.println("A2 : " + A2.exitedCars);
-//            System.out.println("AVG DWELL TIME OF A2: " + getDwellTimes(A2));
-////        System.out.println("A3 : " + A3.exitedCars);
-//            System.out.println("AVG DWELL TIME OF A3: " + getDwellTimes(A3));
-//            System.out.println("AVG DWELL TIME OF B1: " + getDwellTimes(B1));
-//            System.out.println("AVG DWELL TIME OF C1: " + getDwellTimes(C1));
-//            System.out.println("AVG DWELL TIME OF C2: " + getDwellTimes(C2));
-//            System.out.println("AVG DWELL TIME OF D1: " + getDwellTimes(D1));
-//
-//            System.out.println("---------- Cars Left At the End --------");
-//            System.out.println("TOTAL CARS A1: " + A1.totalCar);
-//            System.out.println("TOTAL CARS A2: " + A2.totalCar);
-//            System.out.println("TOTAL CARS A3: " + A3.totalCar);
-//            System.out.println("TOTAL CARS B1: " + B1.totalCar);
-//            System.out.println("TOTAL CARS C1: " + C1.totalCar);
-//            System.out.println("TOTAL CARS C2: " + C2.totalCar);
-//            System.out.println("TOTAL CARS D1: " + D1.totalCar);
-            resetLanes();
+            saveDwellTimes();               // Saves Dwell Times of 1 Simulation in a List
+            resetLanes();                   // Resets Lanes for next Simulation
         }
+        printAllAverageDwellTimes();
 
+    }
+
+    private static void addNewRandomCar(int time) {
+        System.out.println("\n*---- Adding Cars ----*");
+        A1.addCar(time);
+        addToA2(time);                      // this method adds new car to the smaller list between A1 and A2
+        addToA3(time);                      // this method check if there is free space in lane A3 otherwise add it to A2 after index 2.
+        B1.addCar(time);
+        C1.addCar(time);
+        addToC2(time);                      // this method check if the smaller size between C1 and C2 lane and add new car to it.
+        D1.addCar(time);
+        System.out.println();
+    }
+
+    private static void printLanes() {
+        System.out.println("--lanes--");
+        printCars(A1.getCars(), A1.laneName);
+        printCars(A2.getCars(), A2.laneName);
+        printCars(A3.getCars(), A3.laneName);
+        printCars(B1.getCars(), B1.laneName);
+        printCars(C1.getCars(), C1.laneName);
+        printCars(C2.getCars(), C2.laneName);
+        printCars(D1.getCars(), D1.laneName);
+    }
+
+    private static void saveDwellTimes() {
+        dwellA1.add(getDwellTimes(A1));
+        dwellA2.add(getDwellTimes(A2));
+        dwellA3.add(getDwellTimes(A3));
+        dwellB1.add(getDwellTimes(B1));
+        dwellC1.add(getDwellTimes(C1));
+        dwellC2.add(getDwellTimes(C2));
+        dwellD1.add(getDwellTimes(D1));
+    }
+
+    private static void printAllAverageDwellTimes() {
         System.out.print(dwellA1);
         System.out.println(" | Average :" + calculateAverage(dwellA1));
         System.out.print(dwellA2);
@@ -80,9 +93,9 @@ public class Main {
         System.out.println(" | Average :" + calculateAverage(dwellD1));
     }
 
-    public static String calculateAverage(List<Integer> numbers) {
+    public static String calculateAverage(List<Double> numbers) {
         double a = numbers.stream()
-                .mapToDouble(Integer::doubleValue)
+                .mapToDouble(Double::doubleValue)
                 .average()
                 .orElse(0.0);
 
@@ -99,7 +112,7 @@ public class Main {
         D1 =  new Lane("D1", 0.07);
     }
 
-    private static int getDwellTimes(Lane lane) {
+    private static double getDwellTimes(Lane lane) {
         if(lane.exitedCars.size() != 0){
             int LaneDwellTimeSum = 0;
             for(Car car : lane.exitedCars){
@@ -108,7 +121,7 @@ public class Main {
                 LaneDwellTimeSum += dwellTime;
             }
 //        System.out.println();
-            int averageDwellTime = LaneDwellTimeSum / lane.exitedCars.size();
+            double averageDwellTime = LaneDwellTimeSum / lane.exitedCars.size();
             return averageDwellTime;
         }
         return 0;
@@ -152,17 +165,6 @@ public class Main {
         }
     }
 
-    private static void printLanes() {
-        System.out.println("--lanes--");
-        printCars(A1.getCars(), A1.laneName);
-        printCars(A2.getCars(), A2.laneName);
-        printCars(A3.getCars(), A3.laneName);
-        printCars(B1.getCars(), B1.laneName);
-        printCars(C1.getCars(), C1.laneName);
-        printCars(C2.getCars(), C2.laneName);
-        printCars(D1.getCars(), D1.laneName);
-    }
-
     private static void printCars(Queue<Car> cars, String laneName) {
         System.out.println(laneName);
         if (!cars.isEmpty()) {
@@ -172,18 +174,6 @@ public class Main {
             System.out.println();
         }
 
-    }
-
-    private static void addNewRandomCar(int time) {
-        System.out.println("\n*---- Adding Cars ----*");
-        A1.addCar(time);
-        addToA2(time); // this method add new car to smaller list between A1 and A2
-        addToA3(time);  // this method check if there is free space in lane A3 otherwise add it to A2 after index 2.
-        B1.addCar(time);
-        C1.addCar(time);
-        addToC2(time);  // this method check if the smaller size between C1 and C2 lane and add new car to it.
-        D1.addCar(time);
-        System.out.println();
     }
 
     private static void removeCars(int time) {
@@ -245,7 +235,7 @@ public class Main {
                         A2.getCars().add(2, new Car("", "", time)); // and make space by stopping the waiting car at position 4
                     }
                 }
-            } else { // if there is no car waiting to change the lane to A3 then simply remove a car from A2.
+            } else {                                                                         // if there is no car waiting to change the lane to A3 then simply remove a car from A2.
                 A2.removeCar(time);
             }
         }
--- src/test.java
+++ src/test.java
@@ -4,8 +4,28 @@ import java.util.Random;
 
 public class test {
     public static void main(String[] args) {
-        int time = 140;
-        int timeModulo60 = time%60;
-        System.out.println(timeModulo60);
+
+
+        // TO SEE DWELL TIMES IN ONE SIMULATION
+//            System.out.println("----------------- DWELL TIME -------------");
+//            System.out.println("A1 : " + A1.exitedCars);
+//            System.out.println("AVG DWELL TIME OF A1: " + getDwellTimes(A1));
+//            System.out.println("A2 : " + A2.exitedCars);
+//            System.out.println("AVG DWELL TIME OF A2: " + getDwellTimes(A2));
+//            System.out.println("A3 : " + A3.exitedCars);
+//            System.out.println("AVG DWELL TIME OF A3: " + getDwellTimes(A3));
+//            System.out.println("AVG DWELL TIME OF B1: " + getDwellTimes(B1));
+//            System.out.println("AVG DWELL TIME OF C1: " + getDwellTimes(C1));
+//            System.out.println("AVG DWELL TIME OF C2: " + getDwellTimes(C2));
+//            System.out.println("AVG DWELL TIME OF D1: " + getDwellTimes(D1));
+//
+//            System.out.println("---------- Cars Left At the End --------");
+//            System.out.println("TOTAL CARS A1: " + A1.totalCar);
+//            System.out.println("TOTAL CARS A2: " + A2.totalCar);
+//            System.out.println("TOTAL CARS A3: " + A3.totalCar);
+//            System.out.println("TOTAL CARS B1: " + B1.totalCar);
+//            System.out.println("TOTAL CARS C1: " + C1.totalCar);
+//            System.out.println("TOTAL CARS C2: " + C2.totalCar);
+//            System.out.println("TOTAL CARS D1: " + D1.totalCar);
     }
 }
